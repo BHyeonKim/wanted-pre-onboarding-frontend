4e41576e1296f7653872089b649374b7550f2cc9
BHyeonKim <rlaqhguse@gmail.com>
2023-08-02 17:21:58 +0900

    ✨ feat: update api service

--- src/services/todo.ts
+++ src/services/todo.ts
@@ -1,5 +1,10 @@
 import { AxiosPromise } from 'axios'
-import { SignInResponse } from 'types/todo'
+import type {
+  CreateTodoResponse,
+  GetTodosResponse,
+  SignInResponse,
+  UpdateTodoResponse,
+} from 'types/todo'
 import Axios from 'utils/axios'
 
 export default {
@@ -31,6 +36,58 @@ export default {
         email,
         password,
       },
+    })
+  },
+  /**
+   * 투두 생성하는 api
+   * @param todo 투두 내용
+   */
+  createTodo(todo: string): AxiosPromise<CreateTodoResponse> {
+    return Axios({
+      url: '/todos',
+      method: 'post',
+      data: {
+        todo,
+      },
+    })
+  },
+  /**
+   * 투두 리스트를 가져오는 api
+   */
+  getTodos(): AxiosPromise<GetTodosResponse> {
+    return Axios({
+      url: '/todos',
+      method: 'get',
+    })
+  },
+  /**
+   * 투두 아이템을 수정하는 api
+   * @param id 수정하고 싶은 투두 아이템의 아이디
+   * @param todo 수정 될 투두 내용
+   * @param isCompleted 투두 완료 여부
+   */
+  updateTodo(
+    id: number,
+    todo: string,
+    isCompleted: boolean,
+  ): AxiosPromise<UpdateTodoResponse> {
+    return Axios({
+      url: `/todos/${id}`,
+      method: 'put',
+      data: {
+        todo,
+        isCompleted,
+      },
+    })
+  },
+  /**
+   * 투두 아이템을 삭제하는 api
+   * @param id 삭제하고 싶은 투두 아이템의 아이디
+   */
+  deleteTodo(id: number): AxiosPromise {
+    return Axios({
+      url: `/todos/${id}`,
+      method: 'delete',
     })
   },
 }
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+import { getToken } from './localstorage'
+
 const instance = axios.create({
   baseURL: process.env.REACT_APP_BASEURL,
   headers: {
@@ -9,3 +11,16 @@ const instance = axios.create({
 })
 
 export default instance
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = getToken()
+
+    if (token) config.headers.Authorization = `Bearer ${token}`
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
