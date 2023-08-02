dc390f204caa9759c3610b274529febcd15ca40b
BHyeonKim <rlaqhguse@gmail.com>
2023-08-02 17:22:57 +0900

    ✨ feat: add todo page

--- src/pages/TodoPage/index.tsx
+++ src/pages/TodoPage/index.tsx
@@ -1,4 +1,19 @@
+import classNames from 'classnames/bind'
+import TodoInput from 'components/TodoInput'
+import TodoItem from 'components/TodoItem'
+
+import styles from './todo.module.scss'
+
+const cx = classNames.bind(styles)
+
 const TodoPage = () => {
-  return <div>투두</div>
+  return (
+    <div className={cx('page')}>
+      <ul>
+        <TodoItem />
+      </ul>
+      <TodoInput />
+    </div>
+  )
 }
 export default TodoPage
